43d9df58891cbf2a5e6c5e8cd87cda473c2ca0fb
durnevaola <durnevaola50@gmail.com>
2026-05-25 12:43:49 +0700

    Create task_10.py

---
+++ task_10.py
@@ -0,0 +1,18 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@timer
+def heavy_computation(n):
+    return sum(i * i for i in range(n))
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x ** 2, numbers))
+filtered = list(filter(lambda x: x > 10, squared))
